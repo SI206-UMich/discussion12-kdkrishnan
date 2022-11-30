cf923b0f919d489b38c546a9a740bef478cf8526
kdkrishnan <kirandk@umich.edu>
2022-11-30 14:43:17 -0500

    update

--- discussion_12.py
+++ discussion_12.py
@@ -16,7 +16,8 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    pass
+    cur.execute("CREATE TABLE IF NOT EXISTS employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, hire_date TEXT, job_id INTEGER, salary INTEGER)")
+    conn.commit()
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
 
@@ -27,17 +28,34 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
-    pass
+    employee_data = json.loads(file_data)
+    for item in employee_data:
+        employee_id = int(item['employee_id'])
+        first_name = item['first_name']
+        last_name = item['last_name']
+        hire_date = item['hire_date']
+        job_id = int(item['job_id'])
+        salary = int(item['salary'])
+        cur.execute('INSERT INTO employees (employee_id, first_name, last_name, hire_date, job_id, salary) VALUES(?,?,?,?,?,?)',
+        (employee_id, first_name, last_name, hire_date, job_id, salary))
+    conn.commit()
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
-    pass
+    cur.execute('SELECT employees.hire_date, Jobs.job_title FROM employees JOIN Jobs ON employees.job_id = Jobs.job_id')
+    job_hire_date = cur.fetchall()
+    conn.commit()
 
+    sorted_dates = sorted(job_hire_date, key=lambda x: x[0])
+    return sorted_dates[0][1]
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
-    pass
+    cur.execute('SELECT employees.first_name, employees.last_name FROM employees JOIN Jobs ON employees.job_id = Jobs.job_id WHERE employees.salary > Jobs.max_salary OR employees.salary < Jobs.min_salary')
+    invalid = cur.fetchall()
+    conn.commit
 
+    return invalid
 # TASK 4: VISUALIZATION
 def visualization_salary_data(cur, conn):
     pass
